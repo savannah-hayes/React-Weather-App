a4949287dfb50f47f846d35dffc2c7294f17d604
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-05 16:54:38 +0200

    Added Github and LinkedIn Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer class="github-link" id="github-link">
+        <small>
+          Open-source
+          <a
+            href="https://github.com/savannah-hayes/React-Weather-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+          , by Savannah Hayes
+          <a
+            href="http://linkedin.com/in/savannah-hayes-128b0418a"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
