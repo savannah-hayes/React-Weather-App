7d0710d5c7a8c5e1d2e6b68ca864aedd7b56ac14
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-05 17:33:05 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer class="github-link" id="github-link">
-        <small>
-          Open-source
-          <a
-            href="https://github.com/savannah-hayes/React-Weather-App"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i class="fab fa-github"></i>
-          </a>
-          , by Savannah Hayes
-          <a
-            href="http://linkedin.com/in/savannah-hayes-128b0418a"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i class="fab fa-linkedin-in"></i>
-          </a>
-        </small>
-      </footer>
+      <div className="container">
+        <footer class="github-link" id="github-link">
+          <small>
+            Open-source{" "}
+            <a
+              href="https://github.com/savannah-hayes/React-Weather-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+            , by Savannah Hayes{" "}
+            <a
+              href="http://linkedin.com/in/savannah-hayes-128b0418a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
