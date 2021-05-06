efb804ad616e582a8c55320184887ea37fe736b2
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-06 16:10:25 +0200

    Used the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ function Weather(props) {
       humidity: response.data.main.humidity,
       pressure: response.data.main.pressure,
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
